8acc788ef53516688b65006a0e33a881ee1713bf
Felipe Buche <107374380+felipebuchee@users.noreply.github.com>
2024-11-18 11:55:24 -0300

    Add files via upload

---
+++ javascript/fecharMenu.js
@@ -0,0 +1,11 @@
+var formReg = document.querySelector('.form-reg');
+var btnCadt = document.querySelector('.btnCadt');
+var fechar = document.querySelector('.fechar');
+
+btnCadt.addEventListener('click', ()=>{
+    telaLogin.classList.add('ativoTela');
+});
+
+fechar.addEventListener('click', ()=>{
+    telaLogin.classList.remove('ativoTela');
+});
